d4cff991ce72c26afd5dd502509413cbf8fa0332
Michael P <102651473+michmich16@users.noreply.github.com>
2024-10-23 10:31:31 +0200

    update

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -19,19 +19,9 @@ export const MainLayout = () => {
   return (
     <>
       <HelmetProvider>
-        <Helmet>
-          <title>Lexiconline by Michael P</title>
-          <meta name='description' content='Online Dictionary provided for you by Michmich16. Contains more than 1 million words' />
-          <meta name="keywords" content="Dictionary, Search, Meaning, English" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta property='og:title' content='Lexiconline by Michael P' />
-          <meta property='og:url' content='https://lexiconline.netlify.app/' />
-          <meta property="og:description" content="Online Dicktionary provided for you by Michmich16. Contains more than 1 million words' " />
-          <meta property="og:locale" content="en_GB" />
-        </Helmet>
-      <Header />
-      <Outlet />
-      <Footer />
+        <Header />
+        <Outlet />
+        <Footer />
       </HelmetProvider>
     </>
   );
